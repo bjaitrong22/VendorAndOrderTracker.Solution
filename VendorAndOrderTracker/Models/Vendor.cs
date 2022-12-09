af2bd57f43b350d739bdadda7d813a6b15768a21
Bai Jaitrong <bjaitrong@comcast.net>
2022-12-09 15:53:33 -0700

    Add Vendor Class and constructor .

---
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace VendorAndOrderTracker.Models
+{
+  public class Vendor
+  {
+    public string Name;
+    public Vendor(string vendorName)
+    {
+      Name = vendorName;
+    }
+  }
+
+}
