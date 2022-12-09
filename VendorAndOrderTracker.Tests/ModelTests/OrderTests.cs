ac811390a9b3f95502594cfdac23d15559db2749
Bai Jaitrong <bjaitrong@comcast.net>
2022-12-09 10:01:53 -0700

    Add Title property and getter.

--- VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -11,8 +11,24 @@ namespace VendorAndOrderTracker.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order();
+      string title = "Bread Order";
+      
+      Order newOrder = new Order(title);
       Assert.AreEqual(typeof(Order),newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetTitle_ReturnsTitle_String()
+    {
+      //Arrange
+      string title = "Bread Order";
+      Order newOrder = new Order(title);
+
+      //Act
+      string result = newOrder.Title;
+
+      //Assert
+      Assert.AreEqual(title, result);
     }
   }
 }
--- VendorAndOrderTracker/Models/Order.cs
+++ VendorAndOrderTracker/Models/Order.cs
@@ -2,6 +2,12 @@ namespace VendorAndOrderTracker.Models
 {
   public class Order
   {
+    public string Title { get; }
+    
+    public Order(string title)
+    {
+      Title = title;
+    }
     
   }
 }
