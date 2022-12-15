61ea7267c092ef0acab89fe175318490b0d8986f
Bai Jaitrong <bjaitrong@comcast.net>
2022-12-15 16:12:08 -0700

    Add Vendor Controller and View Index.

---
+++ VendorAndOrderTracker/Controllers/VendorsController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorAndOrderTracker.Models;
+
+namespace VendorAndOrderTracker.Controllers
+{
+  public class VendorsController: Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+  }
+}
