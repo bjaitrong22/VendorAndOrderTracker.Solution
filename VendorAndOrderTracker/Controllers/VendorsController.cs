f9a209007c1833d69d4b7fe24548de78990938b9
Bai Jaitrong <bjaitrong@comcast.net>
2022-12-15 22:37:53 -0700

    Update VendorController with route for vendor's orders.

--- VendorAndOrderTracker/Controllers/VendorsController.cs
+++ VendorAndOrderTracker/Controllers/VendorsController.cs
@@ -26,5 +26,29 @@ namespace VendorAndOrderTracker.Controllers
       Vendor newVendor = new Vendor(vendorName, vendorDescription);
       return RedirectToAction("Index");
     }
+
+    [HttpGet("/Vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object> ();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
+
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string OrderTitle, string OrderDescription, decimal orderPrice)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object> ();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order (OrderTitle,OrderDescription, orderPrice);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
   }
 }
